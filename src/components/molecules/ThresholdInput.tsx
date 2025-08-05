4ed0ce429a227b53d1d8f675eea7b21173673d58
Karam Kottish <155135547+Karamkottish@users.noreply.github.com>
2025-08-05 15:38:02 +0300

    Update ThresholdInput.tsx

--- src/components/molecules/ThresholdInput.tsx
+++ src/components/molecules/ThresholdInput.tsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import { Slider } from '../atoms/Slider';
 
+interface ThresholdInputProps {
+  value: number;
+  max: number;
+  onChange: (v: number) => void;
+}
+
+export const ThresholdInput: React.FC<ThresholdInputProps> = ({ value, max, onChange }) => (
+  <div className="w-full">
+    <label className="block mb-1 text-gray-700">Min Sales Threshold</label>
+    <div className="flex items-center space-x-4">
+      <Slider min={0} max={max} value={value} onChange={onChange} />
+      <input
+        type="number"
+        className="w-20 p-1 border rounded"
+        value={value}
+        onChange={e => onChange(Number(e.target.value))}
+      />
+    </div>
+  </div>
+);

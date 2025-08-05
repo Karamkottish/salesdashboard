8054b877ac5924bf301132d9c21e2c962d47de42
Karam Kottish <155135547+Karamkottish@users.noreply.github.com>
2025-08-05 15:35:35 +0300

    Update sales..ts

--- src/data/sales..ts
+++ src/data/sales..ts
@@ -1,1 +1,10 @@
+export interface SalesRecord {
+  year: number;
+  sales: number;
+}
 
+export const salesData: SalesRecord[] = [
+  { year: 2022, sales: 7300 },
+  { year: 2023, sales: 8400 },
+  { year: 2024, sales: 9200 },
+];

60123d457790367f5877eb5419916012a49361da
Karam Kottish <155135547+Karamkottish@users.noreply.github.com>
2025-08-05 15:37:08 +0300

    Update slider.tsx

--- src/components/atoms/slider.tsx
+++ src/components/atoms/slider.tsx
@@ -1,1 +1,19 @@
+import React from 'react';
 
+interface SliderProps {
+  min: number;
+  max: number;
+  value: number;
+  onChange: (v: number) => void;
+}
+
+export const Slider: React.FC<SliderProps> = ({ min, max, value, onChange }) => (
+  <input
+    type="range"
+    min={min}
+    max={max}
+    value={value}
+    onChange={e => onChange(Number(e.target.value))}
+    className="w-full"
+  />
+);
